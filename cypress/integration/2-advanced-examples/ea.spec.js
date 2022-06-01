36b3ad30c63ef5f86376fd8bc4577e3650c950b9
raghavBakshee <raghav_bakshee@yahoo.in>
2022-06-01 21:25:21 +0530

    first committ

---
+++ cypress/integration/2-advanced-examples/ea.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+
+describe("Testing of EA app", () => {
+
+    it("Login Application", () => {
+        cy.visit("http://eaapp.somee.com/");
+
+        cy.contains("Employee");
+        cy.contains("Login").click();
+        cy.url().should("include","/Account/Login")
+        cy.contains("Somee.com");
+        cy.get('#UserName').type("admin");
+        cy.get('#Password').type("password");
+        cy.get('.btn').click();
+        cy.get('#logoutForm > .nav > :nth-child(1) > a').should("have.text","Hello admin!")
+
+    })
+
+})
